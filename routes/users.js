0131843031aaffd00ec7f4339ebbb12c8915d515
Yofriadi Yahya <yofriadiyahya@gmail.com>
2017-12-08 02:46:25 +0700

    Most visited finish

--- routes/users.js
+++ routes/users.js
@@ -24,14 +24,16 @@ module.exports = router
       })
 
       const rowsUser = await User.findById(1, {
-        attributes: ['id', 'name', 'email', 'password', 'role'],
-        include: [{
-          model: Restaurant,
-          attributes: ['id', 'name', 'address', 'territory', 'latitude', 'longitude']
-        }]
+        attributes: ['id', 'name', 'email', 'password', 'role']
       })
 
-      res.render('users/users-home', {rowsRestaurant, rowsUser, lists, bool})
+      const checkedIn = await CheckedIn.findAll({
+        where: {UserId: 1},
+        include: Restaurant,
+        order: [['createdAt', 'DESC']]
+      })
+
+      res.render('users/users-home', {rowsRestaurant, rowsUser, checkedIn, lists, bool})
     } catch (err) {
       console.error(err)
     }
@@ -40,7 +42,56 @@ module.exports = router
     try {
       const rowsRestaurant = await Restaurant.findAll({where: {territory: req.query.territory}})
       res.render('users/checkin-restaurants', {rowsRestaurant})
-      // res.send(rowsRestaurant)
+      res.send(rows)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+  .post('/:id/checkin', async (req, res) => {
+    try {
+      await CheckedIn.create({
+        RestaurantId: req.body.restaurantId,
+        UserId: req.params.id,
+        details: req.body.details
+      })
+      res.redirect('/users')
+    } catch (err) {
+      console.error(err)
+    }
+  })
+  .get('/most-visited', async (req, res) => {
+    try {
+      const rowsRestaurant = await Restaurant.findAll()
+      const rowsCheckedIn = await CheckedIn.findAll()
+
+      const total = rowsRestaurant.length
+      const most = []
+      for (let i = 0; i < total; i++) {
+        const findMost = rowsCheckedIn.filter(restaurant => {
+          return restaurant.RestaurantId === i + 1
+        })
+        most.push(findMost.length)
+      }
+
+      const sorted = []
+      for (let i = 0; i < most.length; i++) {
+        let current = most[i]
+        let j = i
+        while (j < most.length - 1) {
+          if (current < most[j]) current = most[j]
+          j++
+        }
+        if (current === most[i]) sorted.push(i+1)
+      }
+
+
+      const results = []
+      for (let i = 0; i < most.length; i++) {
+        console.log(i)
+        results.push(await Restaurant.findById(i+1))
+      }
+
+      res.render('users/most-visited', {results})
     } catch (err) {
       console.error(err)
     }
